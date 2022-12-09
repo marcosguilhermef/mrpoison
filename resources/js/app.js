1737889c9eba70912cab8f0f13a8581421cdc1fd
marcosguilhermef <marcosguilhermefh@gmail.com>
2022-12-08 21:23:41 -0300

    first

---
+++ resources/js/app.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { createInertiaApp } from '@inertiajs/inertia-react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+//require("./bootstrap")
+
+createInertiaApp({
+  resolve: name => require(`./Pages/${name}`),
+  setup({ el, App, props }) {
+    render(<App {...props} />, el)
+  },
+})
